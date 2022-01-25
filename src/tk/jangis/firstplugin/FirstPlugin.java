bc6b2e78b7232ed8431c946d2ec4876d33756531
BuildTools <unconfigured@null.spigotmc.org>
2022-01-25 22:23:43 +0200

    Backing up some files

--- src/tk/jangis/firstplugin/FirstPlugin.java
+++ src/tk/jangis/firstplugin/FirstPlugin.java
@@ -1,9 +1,11 @@
 package tk.jangis.firstplugin;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class FirstPlugin extends JavaPlugin implements Listener {
+    private FileConfiguration config = this.getConfig();
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new WelcomePlayer(), this);
@@ -12,8 +14,14 @@ public class FirstPlugin extends JavaPlugin implements Listener {
         this.getCommand("rchest").setExecutor(new CommandRChest());
         this.getCommand("sudo").setExecutor(new CommandSudo());
         this.getCommand("web").setExecutor(new CommandSpider());
-    }
+        config.addDefault("DefaultSpiderDis",999999);
+        config.options().copyDefaults(true);
+        this.saveConfig();
 
+
+
+    }
+   public FileConfiguration con = this.getConfig();
     @Override
     public void onDisable() {
 
