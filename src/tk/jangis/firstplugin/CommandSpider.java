8235733da76ec74746d5633d62a6201a1fb939f1
BuildTools <unconfigured@null.spigotmc.org>
2022-01-26 11:54:06 +0200

    Fixed some bugs

--- src/tk/jangis/firstplugin/CommandSpider.java
+++ src/tk/jangis/firstplugin/CommandSpider.java
@@ -15,24 +15,26 @@ public class CommandSpider implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] args) {
         if(commandSender instanceof Player){
                 Player player = (Player) commandSender;
-
-            try {
-                if(args[0].equals("set")){
-                    int distance = Integer.parseInt(args[1]);
-                    FirstPlugin.plugin.getConfig().set(player.getName()+"SpiderDis",distance);
-                }
-            }catch (ArrayIndexOutOfBoundsException e){
-                return true;
-            }
-
+            if(args.length < 1) {
                 FirstPlugin.plugin.getConfig().addDefault(player.getName() + "SpiderDis", 5);
                 FirstPlugin.plugin.getConfig().options().copyDefaults(true);
                 FirstPlugin.plugin.saveConfig();
                 Block block = player.getTargetBlock(null, FirstPlugin.plugin.getConfig().getInt(player.getName() + "SpiderDis"));
                 block.setType(Material.COBWEB);
-
-
                 if (player.getName().equals("SupremeDan")) player.sendMessage("You are pardurmen!!");
+            }else {
+
+                try {
+                    if (args[0].equals("set")) {
+                        int distance = Integer.parseInt(args[1]);
+                        FirstPlugin.plugin.getConfig().set(player.getName() + "SpiderDis", distance);
+                    }
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    return true;
+                }
+            }
+
+
 
 
 
